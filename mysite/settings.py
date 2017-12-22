7d16600d6bfad891bf33201523ce73f735f63458
Krishnakumar01 <kichukripa00@gmail.com>
2017-12-22 19:55:31 +0530

    First

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a@dl53w#xv-as+g^%3j=5ky!%!865xpwi!c7_ex0*-kh+77g81'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','krishnakumar.pythonanywhere.com']
 
 
 # Application definition
